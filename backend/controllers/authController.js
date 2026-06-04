d91b34b8b8aad2d0944d5bcc6ac5907e70ece1fa
Moone <imooneshine@gmail.com>
2026-06-04 13:05:20 +0530

    Updated

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -70,7 +70,8 @@ exports.logout = (req, res) => {
   res.cookie("token", "", {
     httpOnly: true,
     expires: new Date(0),
-    sameSite: "strict",
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
   });
   res.status(200).json({ message: "Logged out successfully" });
 };
